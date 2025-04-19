5752b902e3e4211a33891d426b42dc29b9d1007f
Ahmad Ansori Palembani <palembani@gmail.com>
2025-04-19 07:19:31 +0700

    chore(deps): Update packages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,9 +5,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'ZiROWare Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/null2264',
 			},
 			sidebar: [
 				{
