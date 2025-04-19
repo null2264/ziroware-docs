13a2bd6a96daf45a3be7150f55f89d54644eefa0
Ahmad Ansori Palembani <palembani@gmail.com>
2025-04-19 07:22:45 +0700

    chore: Add .editorconfig

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,25 +3,25 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'ZiROWare Docs',
-			social: {
-				github: 'https://github.com/null2264',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: 'ZiROWare Docs',
+            social: {
+                github: 'https://github.com/null2264',
+            },
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Example Guide', link: '/guides/example/' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
+        }),
+    ],
 });
