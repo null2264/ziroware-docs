2501f2c7eb7571f6f70966df835dc6820f128df7
Ahmad Ansori Palembani <palembani@gmail.com>
2025-04-19 08:09:11 +0700

    chore: Testing 'topics'

---
+++ docs/astro.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "astro/config"
+import starlight from "@astrojs/starlight"
+import starlightSidebarTopics from "starlight-sidebar-topics"
+
+export default defineConfig({
+    integrations: [
+        starlight({
+            title: "ZiROWare Docs",
+            social: {
+                github: "https://github.com/null2264",
+            },
+            plugins: [
+                starlightSidebarTopics(
+                    [
+                        {
+                            label: "Guides",
+                            link: "/guides/example",
+                            icon: "open-book",
+                            items: ["guides/example"],
+                        },
+                        {
+                            id: "reference",
+                            label: "Reference",
+                            link: "/reference/",
+                            icon: "information",
+                            items: [
+                                { label: "Reference", autogenerate: { directory: "reference/test" } },
+                            ],
+                        },
+                    ],
+                ),
+            ],
+        }),
+    ],
+});
